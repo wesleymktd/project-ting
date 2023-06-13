3a992bb7e96151adaa617e675e84ae014ec56126
wesleymktd <wesleymktd@gmail.com>
2023-06-13 15:25:08 -0300

    fix lint

--- ting_file_management/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
- # def __str__(self):
-    #     str_queue = ""
-    #     for index in range(0, self.size()):
-    #         value = self._data[index]
-    #         str_queue += str(value)
-    #         if index + 1 < self.size():
-    #             str_queue += " <- "
-    #     return "FILA: |" + str_queue + "|"
