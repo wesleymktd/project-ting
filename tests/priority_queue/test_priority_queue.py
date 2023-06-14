b0cdb89a90dc59d7571949ffc678d7ad9e93366c
wesleymktd <wesleymktd@gmail.com>
2023-06-14 08:34:49 -0300

    req6 test_priority_queue ok

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,26 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority_queue = PriorityQueue()
+    file_data1 = {"nome_do_arquivo": "aquivo1.txt", "qtd_linhas": 3}
+    file_data2 = {"nome_do_arquivo": "aquivo2.txt", "qtd_linhas": 10}
+    file_data3 = {"nome_do_arquivo": "aquivo3.txt", "qtd_linhas": 6}
+    file_data4 = {"nome_do_arquivo": "aquivo4.txt", "qtd_linhas": 1}
+
+    priority_queue.enqueue(file_data1)
+    priority_queue.enqueue(file_data2)
+    priority_queue.enqueue(file_data3)
+    priority_queue.enqueue(file_data4)
+
+    assert len(priority_queue) == 4
+    assert priority_queue.search(1) == file_data4
+
+    item_dequeue = priority_queue.dequeue()
+
+    assert len(priority_queue) == 3
+    assert item_dequeue == file_data1
+
+    with pytest.raises(IndexError):
+        priority_queue.search(20)
