3c962044b8215d5a1f46cb33ce30ddc9a46d7fbf
wesleymktd <wesleymktd@gmail.com>
2023-06-14 09:36:27 -0300

    função exists_word implementada req7 ok

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+
+    for index in range(len(instance)):
+        file_data = instance.search(index)
+        file_name = file_data.get("nome_do_arquivo")
+        lines = file_data.get("linhas_do_arquivo")
+        findOk = []
+
+        for i, line in enumerate(lines, 1):
+            if word.lower() in line.lower():
+                findOk.append({"linha": i})
+
+        if findOk:
+            result = {
+                "palavra": word,
+                "arquivo": file_name,
+                "ocorrencias": findOk
+            }
+            results.append(result)
+
+    return results
 
 
 def search_by_word(word, instance):
